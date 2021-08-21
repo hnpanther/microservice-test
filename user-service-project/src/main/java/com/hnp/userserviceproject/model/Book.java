bede38197d762eee066d6b58cd4492b8c1a52ced
hnpanther <hadi.hnp@gmail.com>
2021-08-21 19:29:43 +0430

    add Book and Author model to book-service and user-service

---
+++ user-service-project/src/main/java/com/hnp/userserviceproject/model/Book.java
@@ -0,0 +1,60 @@
+package com.hnp.userserviceproject.model;
+
+public class Book {
+
+    private int id;
+    private String name;
+    private int numberOfPage;
+    private Author author;
+
+    public Book() {}
+
+    public Book(int id, String name, int numberOfPage, Author author) {
+        this.id = id;
+        this.name = name;
+        this.numberOfPage = numberOfPage;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfPage() {
+        return numberOfPage;
+    }
+
+    public void setNumberOfPage(int numberOfPage) {
+        this.numberOfPage = numberOfPage;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", numberOfPage=" + numberOfPage +
+                ", author=" + author +
+                '}';
+    }
+}
