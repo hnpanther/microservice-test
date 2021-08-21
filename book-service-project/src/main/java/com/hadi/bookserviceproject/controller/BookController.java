5cead62fd97ec77fa3f4968ab84fc8838ca43fc4
hnpanther <hadi.hnp@gmail.com>
2021-08-21 20:23:46 +0430

    test comminucation between user-service and book-service

--- book-service-project/src/main/java/com/hadi/bookserviceproject/controller/BookController.java
+++ book-service-project/src/main/java/com/hadi/bookserviceproject/controller/BookController.java
@@ -1,8 +1,17 @@
 package com.hadi.bookserviceproject.controller;
 
+import com.hadi.bookserviceproject.model.Book;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class BookController {
@@ -18,11 +27,33 @@ public class BookController {
     @Value("${detail.book}")
     private String detailBook;
 
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
     @GetMapping("/config")
     public String getConfig() {
         String s = "port: " + port + "\ndetail.name: " + detailName +
                 "\ndetail.app: " + detailApp +"\ndetail.gloal: " + detailGlobal +
                 "\ndetail.book: " + detailBook;
         return s;
+    }
+
+    @GetMapping("/test")
+    public String test() {
+//        Application application = this.eurekaClient.getApplication("user-service");
+//        InstanceInfo instanceInfo = application.getInstances().get(1);
+//        System.out.println(instanceInfo.getInstanceId());
+//        System.out.println(instanceInfo.getAppName());
+//        System.out.println(instanceInfo.getIPAddr());
+//        System.out.println(instanceInfo.getPort());
+
+        String url = "http://user-service/books/author";
+        Book book = this.restTemplate.getForObject(url, Book.class);
+        System.out.println(book);
+        return null;
     }
 }
--- user-service-project/src/main/java/com/hnp/userserviceproject/controller/UserController.java
+++ user-service-project/src/main/java/com/hnp/userserviceproject/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.hnp.userserviceproject.controller;
 
+import com.hnp.userserviceproject.model.Author;
+import com.hnp.userserviceproject.model.Book;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -22,5 +29,35 @@ public class UserController {
         String s = "port: " + port + "\ndetail.name: " + detailName +
                 "\ndetail.app: " + detailApp +"\ndetail.gloal: " + detailGlobal;
         return s;
+    }
+
+    @GetMapping("/books/author")
+    public Book getBooksOfAuthor(@RequestBody(required = false) Author author) {
+        System.out.println(author);
+
+        Book book1 = new Book();
+        book1.setId(1);
+        book1.setName("book1");
+        book1.setNumberOfPage(100);
+        book1.setAuthor(author);
+
+        Book book2 = new Book();
+        book2.setId(1);
+        book2.setName("book2");
+        book2.setNumberOfPage(150);
+        book2.setAuthor(author);
+
+        List<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
+        return book1;
+    }
+
+    @GetMapping("/author")
+    public Author getAuthor() {
+        Author author = new Author();
+        author.setId(1);
+        author.setName("authorName");
+        return author;
     }
 }
